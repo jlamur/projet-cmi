f39bbd8bc87da72cec6cb0d145ccc240c89e79a1
Mattéo Delabre <contact@matteodelabre.me>
2016-03-08 18:07:21 +0100

    Héritage de Ball depuis Object et nettoyage de ball.cpp

--- ball.hpp
+++ ball.hpp
@@ -1,38 +1,28 @@
 #ifndef PTF_BALL_HPP
 #define PTF_BALL_HPP
 
+#include "object.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-class Ball {
+class Ball : public Object {
 protected:
-    sf::Vector2f position;
     sf::Vector2f velocity;
     float mass;
-    int charge;
 
 public:
     Ball();
 
-    void update(float delta);
+    /**
+     * Mise à jour de la position de la balle en fonction des forces
+     * qui lui sont appliquées
+     */
+    void update(sf::Vector2f forces, float delta);
+
+    /**
+     * Dessine la balle dans la fenêtre donnée
+     */
     void draw(sf::RenderWindow& window);
-
-    // getters et setters
-    sf::Vector2f getPosition() {
-        return position;
-    }
-
-    sf::Vector2f getVelocity() {
-        return velocity;
-    }
-
-    float getMass() {
-        return mass;
-    }
-
-    int getCharge() {
-        return charge;
-    }
 };
 
 #endif
