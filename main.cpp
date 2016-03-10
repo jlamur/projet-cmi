284fb422a111a6f076c6681686dd6acef05178f2
Mattéo Delabre <contact@matteodelabre.me>
2016-03-10 21:58:30 +0100

    Pas de suivi des fichiers compilés

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include "ball.hpp"
+#include "block.hpp"
 #include "engine.hpp"
+#include <iostream>
 
 int main() {
     Engine engine;
+
     Ball ball(0, 0);
+    std::vector<Block> blocks;
 
     engine.addObject(&ball);
-    engine.start();
 
+    for (int i = 0; i < 10; i++) {
+        Block block(i, 5);
+        std::cout << "Pos: " << block.getPosition().x << " x " << block.getPosition().y << std::endl;
+
+        blocks.push_back(block);
+        engine.addObject(&block);
+    }
+
+    engine.start();
     return 0;
 }
