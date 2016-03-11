41d5c3696cfca0d67220527ac24d4a8c1eb32ad8
Mattéo Delabre <contact@matteodelabre.me>
2016-03-11 16:15:27 +0100

    Affichage des blocs aimantés en couleur

--- block.cpp
+++ block.cpp
@@ -1,6 +1,14 @@
 #include "block.hpp"
 
 void Block::draw(sf::RenderWindow& window) {
+    if (charge > 0) {
+        shape.setFillColor(sf::Color(0, 0, 255));
+    } else if (charge < 0) {
+        shape.setFillColor(sf::Color(255, 0, 0));
+    } else {
+        shape.setFillColor(sf::Color(0, 0, 0));
+    }
+
     shape.setPosition(position);
     window.draw(shape);
 }
