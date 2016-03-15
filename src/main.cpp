6a34c31ae91451d87b89088f8c693fa953d89ea2
Mattéo Delabre <contact@matteodelabre.me>
2016-03-15 21:59:30 +0100

    Amélioration du test pour les collisions

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,8 @@
 int main() {
     Engine engine;
 
-    Ball ball1(5 * Constants::GRID, 1 * Constants::GRID);
-    Ball ball2(7 * Constants::GRID, 1 * Constants::GRID);
-
-    Block block1(2 * Constants::GRID, 7 * Constants::GRID);
+    Ball ball(3 * Constants::GRID, 1 * Constants::GRID);
+    Block block1(3 * Constants::GRID, 6 * Constants::GRID);
     Block block2(3 * Constants::GRID, 7 * Constants::GRID);
     Block block3(4 * Constants::GRID, 7 * Constants::GRID);
     Block block4(5 * Constants::GRID, 7 * Constants::GRID);
@@ -18,14 +16,11 @@ int main() {
     Block block6(7 * Constants::GRID, 7 * Constants::GRID);
     Block block7(8 * Constants::GRID, 7 * Constants::GRID);
     Block block8(9 * Constants::GRID, 7 * Constants::GRID);
-    Block block9(10 * Constants::GRID, 7 * Constants::GRID);
+    Block block9(9 * Constants::GRID, 6 * Constants::GRID);
 
-    ball1.setCharge(-2.f);
-    ball2.setCharge(-2.f);
+    ball.setCharge(-2.f);
     block5.setCharge(16.f);
 
-    engine.addObject(ball1);
-    engine.addObject(ball2);
     engine.addObject(block1);
     engine.addObject(block2);
     engine.addObject(block3);
@@ -35,6 +30,7 @@ int main() {
     engine.addObject(block7);
     engine.addObject(block8);
     engine.addObject(block9);
+    engine.addObject(ball);
 
     engine.start();
     return 0;
