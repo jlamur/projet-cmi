a8e325cb2042faf10e344e7bd9c892f6bab7171c
Mattéo Delabre <contact@matteodelabre.me>
2016-04-02 15:06:21 +0200

    Capture des exceptions pour la lecture du fichier

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,16 @@ int main() {
     Manager manager;
     std::shared_ptr<Game> game = std::shared_ptr<Game>(new Game(manager));
 
-    // ouverture du niveau
-    std::ifstream file;
-    file.open("./levels/level1.dat", std::ios::binary | std::ios::in);
-    game->load(file);
-    file.close();
-
-    manager.setView(game);
-
     try {
+        // ouverture du niveau
+        std::ifstream file;
+        file.open("./levels/level1.dat", std::ios::binary | std::ios::in);
+        game->load(file);
+        file.close();
+
+        manager.setView(game);
         manager.start();
     } catch (const std::exception& exception) {
-        std::cerr << std::endl;
         std::cerr << "Le programme a quitté après une erreur d'exécution." << std::endl;
         std::cerr << exception.what() << std::endl;
         return EXIT_FAILURE;
